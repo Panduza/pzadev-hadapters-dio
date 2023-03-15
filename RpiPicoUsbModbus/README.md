fd7cc644a3d578fbfb7dd0dba0c0ba1212890b2e
paulhfisher <65350546+paulhfisher@users.noreply.github.com>
2023-03-15 11:35:28 +0100

    Pull request Paul Fisher MODBUS (#1)
    * fix bug on git repo, fork has been redone
    
    * add .gitignore
    
    * modify cmake to put usb personalized ID
    
    * add internal LED GPIO
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * clean project repo
    
    * dete old files
    
    * Update README.md
    
    * continue clean of repo
    
    * Create test.md
    
    * Update test.md
    
    * Update test.md
    
    * Update test.md
    
    * Update README.md
    
    * add improvment in Cmakelists.txt, to not put the path when include a header file
    
    * Update README.md
    
    * improve python script before pull request
    
    ---------
    
    Co-authored-by: paul <paul.fisher@elsys-design.com>

---
+++ RpiPicoUsbModbus/README.md
@@ -0,0 +1,74 @@
+# verify PICO_SDK_PATH is not set
+
+This may take some time..
+
+export "PICO_SDK_PATH"="" -> not set SDK path
+
+echo "$PICO_SDK_PATH" -> check it's not set
+
+# build and run project
+
+mkdir build
+
+cd build
+
+cmake ../project
+
+make
+
+cp file_name.uf2 /media/user_name/RPI-RP2/
+
+
+# Notes
+
+GPIO's 4 and 5 not used due to conflit probability with the uart line
+
+
+# PIN configuration
+
+| GPIO | value | description |  returned value |
+|----- |-------|-------------|-----------------|
+|  0   |   1    |      input pull up       |      0x1           | 
+|  1   |   0    |    output         |   0x0              |
+|  2   |   0    |   input pull down          |    0x2             |
+|  3   |  1     |    output         |     0x0            | 
+|  6   |   1    |    input pull up         |    0x1             |
+|  7   |   0    |    output         |   0x0              |
+|  8   |   0   |      output       |    0x0             | 
+|  9   |   0    |     input pull down        |     0x2            |
+|  10   |  1     |    output         |      0x0           |
+|  11   |   1    |    input pull up         |    0x1             | 
+|  12  |   0    |    output         |    0x0             |
+|  13   |  0     |    input pull down         |    0x2             |
+|  14   |  1     |   output          |        0x0         | 
+|  15   |  1     |   input pull up          |  0x1               |
+|  16   |  0     |   output          |      0x0           |
+|  17   |  0     |   input pull down          |   0x2              | 
+|  18   |  1     |   output          |      0x0           |
+|  19   |  1     |   input pull up          |  0x1               |
+|  20   |  0     |   output          |     0x0            | 
+|  21   |  0     |   input pull down          |  0x2               |
+|  22   |  1     |    output         |     0x0            |
+|  25 (INTERNAL LED)  | 1     |    output         |   0x0             | 
+|  26   |     1  |   input pull up          |      0x1           |
+|  27   |   1    |   output          |    0x0             |
+|  28   |   0    |     input pull down        |     0x2            | 
+
+
+
+# note
+
+In code, data is visible in the logs of python script (master.py).
+
+Value 1 is represented by true and 0 by false
+
+
+# Environment requirements
+
+Please make sure you have the following packages :
+
+cmake : sudo apt install cmake
+pymodbus : pip3 install pymodbus
+(recommended) minicom : sudo apt install minicom
+
+Test screens in test.md file
---
+++ RpiPicoUsbModbus/master.py
@@ -0,0 +1,103 @@
+from pymodbus.client import ModbusSerialClient
+from pymodbus.transaction import ModbusRtuFramer
+import logging
+
+FORMAT = ('%(message)-15s')
+logging.basicConfig(format=FORMAT)
+log = logging.getLogger()
+log.setLevel(logging.DEBUG)
+
+
+master = ModbusSerialClient(framer=ModbusRtuFramer, port = '/dev/ttyACM0', stopbits=1, bytesize=8, parity='N', baudrate=115200)
+SLAVE_ID = 0x01
+
+
+connexion = master.connect()
+print(master)
+print("\033[92m-------------------client connected----------------------\033[0m")
+
+
+print("\033[92m-------------------getting PZA----------------------\033[0m")
+getMagic = master.read_holding_registers(0, 2, SLAVE_ID)
+print(getMagic.__dict__)
+print("\033[92m-------------------getting mode----------------------\033[0m")
+getMode = master.read_holding_registers(2, 1, SLAVE_ID)
+print(f"\033[92m there are in mode {getMode.registers[0]}, in I/O mode \033[0m")
+print("\033[92m-------------------getting number of I/O----------------------\033[0m")
+getIO = master.read_holding_registers(3, 1, SLAVE_ID)
+print(f"\033[92m there are {getIO.registers[0]} I/O's \033[0m")
+
+
+
+
+print("\033[92m-------------------GET THE TYPES----------------------\033[0m")
+
+# GPIO 0 to 3
+for i in range(4):
+    print(f"\033[92m-------------------getting type GPIO {i} ----------------------\033[0m")
+    getType0 = master.read_holding_registers(10+i, 1, SLAVE_ID)
+    print("\033[92m value of register \033[0m",getType0.registers)
+    if getType0.registers[0] == 0:
+        print("\033[92m the I/O is a output \033[0m")
+    elif getType0.registers[0]  == 1:
+        print("\033[92m the I/O is a input pull up \033[0m")
+    else:
+        print("\033[92m the I/O is a input pull down \033[0m")
+
+# GPIO 6 to 22
+for i in range(17):
+    print(f"\033[92m-------------------getting type GPIO {i+6} ----------------------\033[0m")
+    getType0 = master.read_holding_registers(16+i, 1, SLAVE_ID)
+    print("\033[92m value of register \033[0m",getType0.registers)
+    if getType0.registers[0]  == 0:
+        print("\033[92m the I/O is a output \033[0m")
+    elif getType0.registers[0]  == 1:
+        print("\033[92m the I/O is a input pull up \033[0m")
+    else:
+        print("\033[92m the I/O is a input pull down \033[0m")
+
+# GPIO 25 to 28
+for i in range(4):
+    print(f"\033[92m-------------------getting type GPIO {i+25} ----------------------\033[0m")
+    getType0 = master.read_holding_registers(35+i, 1, SLAVE_ID)
+    print("\033[92m value of register \033[0m",getType0.registers)
+    if getType0.registers[0]  == 0:
+        print("\033[92m the I/O is a output \033[0m")
+    elif getType0.registers[0]  == 1:
+        print("\033[92m the I/O is a input pull up \033[0m")
+    else:
+        print("\033[92m the I/O is a input pull down \033[0m")
+
+
+# print("\033[92m-------------------READ COILS !!!!!!----------------------\033[0m")
+
+# # GPIO 0 to 3
+# for i in range(4):
+#     print(f"\033[92m-------------------I/O {i}----------------------\033[0m")
+#     getIO_0 = master.read_coils(i, 1, SLAVE_ID)
+#     print("\033[91m DEBUG \033[0m", getIO_0.__dict__)
+#     if getIO_0.bits[0] == False:
+#         print("\033[92m value of coil is 0 \033[0m")
+#     else:
+#         print("\033[92m value of coil is 1 \033[0m")
+
+# # GPIO 6 to to 22
+# for i in range(17):
+#     print(f"\033[92m-------------------I/O {i+6}----------------------\033[0m")
+#     getIO_0 = master.read_coils(i+6, 1, SLAVE_ID)
+#     print("\033[91m DEBUG \033[0m", getIO_0.__dict__)
+#     if getIO_0.bits[0] == False:
+#         print("\033[92m value of coil is 0 \033[0m")
+#     else:
+#         print("\033[92m value of coil is 1 \033[0m")
+
+
+# # GPIO 25 to 28
+# for i in range(4):
+#     print(f"\033[92m-------------------I/O {i+25}----------------------\033[0m")
+#     getIO_0 = master.read_coils(i+25, 1, SLAVE_ID)
+#     print("\033[91m DEBUG \033[0m", getIO_0.__dict__)
+#     if getIO_0.bits[0] == False:
+#         print("\033[92m value of coil is 0 \033[0m")
+#     else:
+#         print("\033[92m value of coil is 1 \033[0m")
